20e2b70d025253bd0638539724287d131769b6ca
Vineeth Kumar <kumarvineeth14@gmail.com>
2019-07-18 19:08:06 +0530

    Update dates.py for the autumn 2019-20

--- dates.py
+++ dates.py
@@ -7,11 +7,11 @@ import pytz
 SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
 #  SEM_BEGIN=build_event.generateIndiaTime(2017, 7, 16, 0, 0)
 
-MID_TERM_BEGIN=build_event.generateIndiaTime(2018, 9, 17, 0, 0)
+MID_TERM_BEGIN=build_event.generateIndiaTime(2019, 9, 16, 0, 0)
 
-MID_TERM_END=build_event.generateIndiaTime(2018, 9, 26, 23, 59)
+MID_TERM_END=build_event.generateIndiaTime(2019, 9, 24, 23, 59)
 
-END_TERM_BEGIN=build_event.generateIndiaTime(2018, 11, 16, 0, 0)
+END_TERM_BEGIN=build_event.generateIndiaTime(2019, 11, 18, 0, 0)
 
 ## Sanity check
 
